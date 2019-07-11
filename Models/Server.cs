ef0d71cd0474301b45126bbd74f84bd9e8709b30
Gerard Browne <gerardbrowne92@gmail.com>
2019-07-11 11:25:02 +1000

    Initial commit

---
+++ Models/Server.cs
@@ -0,0 +1,9 @@
+namespace WAPI.Models
+{
+    public class Server
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool isOnline { get; set; }
+    }
+}
