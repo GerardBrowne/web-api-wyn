60bb408a5a68dc373276affbfc7ab663f1faa942
Gerard Browne <gerardbrowne92@gmail.com>
2019-07-13 18:28:34 +1000

    Removed unused dependencies, modified startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using WAPI.Models;
 
 namespace WAPI
@@ -55,7 +48,9 @@ namespace WAPI
 
             app.UseHttpsRedirection();
             seed.SeedData(20, 1000);
-            app.UseMvc();
+            app.UseMvc(routes => routes.MapRoute(
+                "default", "api/{controller}/{action}/{id?}"
+            ));
         }
     }
 }
